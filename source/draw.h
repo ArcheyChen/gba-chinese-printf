3839053cf306d35ce72e188ee8479a45ab3af172
Ausar <464270342@qq.com>
2023-03-31 23:07:19 +0800

    试图封装printf
    Signed-off-by: Ausar <464270342@qq.com>

--- source/draw.h
+++ source/draw.h
@@ -6,5 +6,5 @@ void Clear(u16 x, u16 y, u16 w, u16 h, u16 c);
 void ClearWithBG(u16* pbg,u16 x, u16 y, u16 w, u16 h);
 void DrawPic(u16 *GFX, u16 x, u16 y, u16 w, u16 h, u8 isTrans, u16 tcolor);
 void DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 c);
-void DEBUG_printf(const char *format, ...);
+void printf_zh(const char *format, ...);
 void ShowbootProgress(char *str);
